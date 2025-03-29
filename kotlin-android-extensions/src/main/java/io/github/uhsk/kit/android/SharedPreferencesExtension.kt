17140067d8ced33abde3b4cca4e29e2011d1c71a
sollyu <sollyu@qq.com>
2025-03-29 16:29:46 +0800

    :sparkles: add SharedPreferences some extension

--- kotlin-android-extensions/src/main/java/io/github/uhsk/kit/android/SharedPreferencesExtension.kt
+++ kotlin-android-extensions/src/main/java/io/github/uhsk/kit/android/SharedPreferencesExtension.kt
@@ -18,6 +18,7 @@
 package io.github.uhsk.kit.android
 
 import android.content.SharedPreferences
+import androidx.core.content.edit
 import org.json.JSONObject
 
 /**
@@ -46,3 +47,124 @@ fun SharedPreferences.Editor.putJson(key: String, value: JSONObject): SharedPref
     this.putString(key, value.toString())
     return this
 }
+
+/**
+ * @since 1.0.14
+ * @author sollyu
+ */
+fun SharedPreferences.getStringOrNull(key: String): String? {
+    return if (has(key)) {
+        getString(key, null)
+    } else {
+        null
+    }
+}
+
+/**
+ * @since 1.0.14
+ * @author sollyu
+ */
+fun SharedPreferences.putStringOrRemove(key: String, value: String?) {
+    if (value == null) {
+        edit { remove(key) }
+    } else {
+        edit { putString(key, value) }
+    }
+}
+
+/**
+ * @since 1.0.14
+ * @author sollyu
+ */
+fun SharedPreferences.getIntOrNull(key: String): Int? {
+    return if (has(key)) {
+        getInt(key, 0)
+    } else {
+        null
+    }
+}
+
+/**
+ * @since 1.0.14
+ * @author sollyu
+ */
+fun SharedPreferences.putIntOrRemove(key: String, value: Int?) {
+    if (value == null) {
+        edit { remove(key) }
+    } else {
+        edit { putInt(key, value) }
+    }
+}
+
+/**
+ * @since 1.0.14
+ * @author sollyu
+ */
+fun SharedPreferences.getLongOrNull(key: String): Long? {
+    return if (has(key)) {
+        getLong(key, 0L)
+    } else {
+        null
+    }
+}
+
+/**
+ * @since 1.0.14
+ * @author sollyu
+ */
+fun SharedPreferences.putLongOrRemove(key: String, value: Long?) {
+    if (value == null) {
+        edit { remove(key) }
+    } else {
+        edit { putLong(key, value) }
+    }
+}
+
+/**
+ * @since 1.0.14
+ * @author sollyu
+ */
+fun SharedPreferences.getFloatOrNull(key: String): Float? {
+    return if (has(key)) {
+        getFloat(key, 0f)
+    } else {
+        null
+    }
+}
+
+/**
+ * @since 1.0.14
+ * @author sollyu
+ */
+fun SharedPreferences.putFloatOrRemove(key: String, value: Float?) {
+    if (value == null) {
+        edit { remove(key) }
+    } else {
+        edit { putFloat(key, value) }
+    }
+}
+
+/**
+ * @since 1.0.14
+ * @author sollyu
+ */
+fun SharedPreferences.getBooleanOrNull(key: String): Boolean? {
+    return if (has(key)) {
+        getBoolean(key, false)
+    } else {
+        null
+    }
+}
+
+/**
+ * @since 1.0.14
+ * @author sollyu
+ */
+fun SharedPreferences.putBooleanOrRemove(key: String, value: Boolean?) {
+    if (value == null) {
+        edit { remove(key) }
+    } else {
+        edit { putBoolean(key, value) }
+    }
+}
+
