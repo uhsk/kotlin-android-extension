6f5ae075975eb1b10f6c933195d3083068891e35
Sollyu <sollyu@qq.com>
2025-04-10 22:41:19 +0800

    :sparkles: add long some extension

--- kotlin-android-extensions/src/main/java/io/github/uhsk/kit/LongExtension.kt
+++ kotlin-android-extensions/src/main/java/io/github/uhsk/kit/LongExtension.kt
@@ -1,10 +1,12 @@
 package io.github.uhsk.kit
 
-import java.util.*
+import java.util.Date
 
 /**
  * 时间戳转日期
  * @since 1.0.2
  * @author ethan
  */
-fun Long.toDate(): Date = Date(this)
+fun Long.toDate(unix: Boolean = false): Date {
+    return Date(if (unix) this * 1000 else this)
+}
